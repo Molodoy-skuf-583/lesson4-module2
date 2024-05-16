861bae269d73c51c1db3e3d9edc1f240e8426d42
Ivan <165202411+Molodoy-skuf-583@users.noreply.github.com>
2024-05-16 13:52:57 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,22 @@
+Cars_ = ["BMW ", "MB ", "LADA ", "KIA ", "HONDA "]
+cars_count_ = 0
+for i in range(len(Cars_)):
+    mix_ = (f'I draw a brand car {Cars_[i]}')
+    print(mix_)
+    cars_count_ += 10
+    sum_ = (f'{cars_count_}')
+    print(sum_)
+
+
+
+
+
+
+    # print(Cars_[i])
+
+#
+#     mix_ += Cars_[i]
+#
+# print(mix_)
+
+
